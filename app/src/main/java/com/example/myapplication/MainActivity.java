57b55c41f5a01be4d674752a1514009b670404ed
Saikat <thisismesaikat@gmail.com>
2021-12-31 02:49:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display = (ImageView) findViewById(R.id.display);
+        String url = "https://www.youtube.com/watch?v=3NBP7nlQEB8"; //put your url here
+        String videoId = url.split("v=")[1];
+
+        String thumbnailMq = "https://img.youtube.com/vi/"+videoId+"/mqdefault.jpg"; //medium quality thumbnail
+
+        String thumbnailHq = "https://img.youtube.com/vi/"+videoId+"/hqdefault.jpg"; //high quality thumbnail
+
+        //Picasso.get().load("http://img.youtube.com/vi/-OKrloDzGpU/mqdefault.jpg").into(display);
+        //display.setText(thumbnailHq);
+        //String imageUri = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.get().load(thumbnailMq).into(display);
+    }
+}
